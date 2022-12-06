732441cf918d0a6dd8aed3166f5b113e4a3cb48d
Kirschall12 <119855431+Kirschall12@users.noreply.github.com>
2022-12-06 12:42:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,11 @@
 import streamlit as st
 st.title('Erste App')
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
