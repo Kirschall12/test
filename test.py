341ee954f6ecfcaaaec489f6de79c13b8df5a124
Kirschall12 <119855431+Kirschall12@users.noreply.github.com>
2022-12-06 12:53:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ st.title('Erste App')
 import matplotlib.pyplot as plt
 import numpy as np
 
+sel1 = st.selectbox('Selection', [1,2,3])
+
+st.write('Deine Auswahl: ', sel1)
+
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
