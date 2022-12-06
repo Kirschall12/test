8692305e9fa6271c6779ef29f85d29815695d843
Kirschall12 <119855431+Kirschall12@users.noreply.github.com>
2022-12-06 13:33:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,26 +5,33 @@
 
 
 import streamlit as st
-st.title('Erste App')
+#st.title('Erste App')
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-sel1 = st.selectbox('Selection', [1,2,3])
+#sel1 = st.selectbox('Selection', [1,2,3])
 
-st.write('Deine Auswahl: ', sel1)
+#st.write('Deine Auswahl: ', sel1)
 
-x = np.linspace(0, 50, 50)
-fig, ax = plt.subplots()
+#x = np.linspace(0, 50, 50)
+#fig, ax = plt.subplots()
 
-ax.plot(x, x**sel1)
-st.pyplot(fig)
+#ax.plot(x, x**sel1)
+#st.pyplot(fig)
 
 
-df = pd.read_csv('Bastar Craton.csv')
+#df = pd.read_csv('Bastar Craton.csv')
 
-sel2 = st.selectbox('Selection', ['Mg', 'Si'])
+#sel2 = st.selectbox('Selection', ['Mg', 'Si'])
+#sel3 = st.selectbox('Selection2', ['Mg', 'Si'])
+#ax.scatter(df[sel2], df[sel3])
+#st.pyplot(fig)
+
+df = pd.read_csv('Bastar Craton')
+x = range(46)
+#sel2 = st.selectbox('Selection', ['Mg', 'Si'])
 sel3 = st.selectbox('Selection2', ['Mg', 'Si'])
-ax.scatter(df[sel2], df[sel3])
+ax.scatter(x, df[sel3])
 st.pyplot(fig)
